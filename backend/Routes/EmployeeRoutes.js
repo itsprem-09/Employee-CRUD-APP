4277337a3f346fd2b54db6358ff54e5a63fb3421
Prem Jani <143021268+itsprem-09@users.noreply.github.com>
2024-10-10 12:19:23 +0530

    Add files via upload

---
+++ backend/Routes/EmployeeRoutes.js
@@ -0,0 +1,18 @@
+const { createEmployee,
+    getAllEmployees,
+    getEmployeeById,
+    deleteEmployeeById,
+    updateEmployeeById,
+    getCount
+} = require('../Controllers/EmployeeController');
+const { cloudinaryFileUploader } = require('../Middlewares/FileUplaoder');
+
+const router = require('express').Router();
+
+router.get('/', getAllEmployees)
+router.get('/:id', getEmployeeById)
+router.delete('/:id', deleteEmployeeById)
+router.put('/:id', cloudinaryFileUploader.single('profileImage'), updateEmployeeById)
+router.post('/', cloudinaryFileUploader.single('profileImage'), createEmployee);
+
+module.exports = router;
